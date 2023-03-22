9f1108d467054ab2718ac694fae7cbbc26d54343
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-03-22 16:59:34 +0000

    webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Yuri G
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function is for calculating the area of a triangle
+ */
+function calculate() {
+ // input
+  const base = parseInt(document.getElementById('base-of-triangle').value)
+  const height = parseInt(document.getElementById('height-of-triangle').value)
+
+  // process
+  const area = (base * height) / 2
+
+  // output
+  document.getElementById ('base-and-height').innerHTML = "The Area is: " + area + " cm²
+}
